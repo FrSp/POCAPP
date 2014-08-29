75ac79b2f4d5acbfeb4f969e61a159609406e8cd
FrSp <fredrik.sparre@seb.se>
2014-08-29 13:42:57 +0200

    The very usefull function added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp
             Console.WriteLine("world");
             Console.WriteLine(Environment.UserName);
             Console.WriteLine("Feature added in branch");
+            Console.WriteLine("Extreamilly usefull function");
             Console.ReadLine();
         }
     }
